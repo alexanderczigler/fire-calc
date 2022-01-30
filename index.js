3db9b344a579387e81584907e772e4aa9a140445
Alexander Czigler <dev@ilix.se>
2022-01-30 14:44:41 +0100

    feat: show tables of quality/survival costs

--- index.js
+++ index.js
@@ -12,19 +12,29 @@ scenario.costs.quality.forEach(({ cost }) => {
   }
 })
 
-const qualitycosts = calculator.summonthly(scenario.costs.quality)
-const survivalcosts = calculator.summonthly(scenario.costs.survival)
-const totalcosts = qualitycosts + survivalcosts
+const qualityCosts = calculator.summonthly(scenario.costs.quality)
+const survivalCosts = calculator.summonthly(scenario.costs.survival)
+const totalcosts = qualityCosts + survivalCosts
 
 console.log(`Welcome to FIRE Calc!`);
 console.log(`-> Loaded scenario ${scenario.name}`)
 console.log()
 
-print.costsTable(scenario.costs.quality, 'Quality')
+console.log()
+console.log(print.bold('Survival costs'))
+console.log('Here is an overview of the costs you need to survive.')
+console.log()
+print.costsTable(scenario.costs.survival, survivalCosts)
+
+console.log()
+console.log(print.bold('Quality-of-Life costs'))
+console.log('In addition to surviving, here is what you need to spend for an enjoyable life.')
+console.log()
+print.costsTable(scenario.costs.quality, qualityCosts)
 
 const fourPercentRequirement = totalcosts * 12 / 0.04;
 
 console.log()
-console.log(`-> Your monthly costs add up to ${print.currency(totalcosts)}`)
-console.log(`-> To be able to withdraw 4 % you will need to have at least ${print.currency(fourPercentRequirement)} invested`)
+console.log(`-> Your monthly costs add up to ${print.bold(print.currency(totalcosts))}`)
+console.log(`-> To be able to withdraw 4 % you will need to have at least ${print.bold(print.currency(fourPercentRequirement))} invested`)
 console.log()
